253c314caf33bec0c26ed2d67d1bf2a2227c30c1
teo <teodor.cotet@gmail.com>
2019-05-14 01:22:39 +0300

    char emb

--- models.py
+++ models.py
@@ -38,11 +38,15 @@ class Model:
     # filename is an elasticsearch dump, use npm elasticdump
     MAX_SENT_TOKENS = 18
     MAX_CHARS_TOKENS = 30
+    MAX_ALLOWED_CHAR = 600
+    # odd
+    WIN_CHARS = 29
     GRU_CELL_SIZE = 64
     PATIENCE = 8
     EPOCHS = 100
     BATCH_SIZE = 64
     DENSES = [128]
+    EMB_CHARS_SIZE = 28
 
 
     def __init__(self): 
@@ -60,8 +64,8 @@ class Model:
         with open(filename, "r") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for jj, row in enumerate(csv_reader):
-                inn = row[0].lower()
-                out = row[1].lower()
+                inn = row[1].lower()
+                out = row[0].lower()
                 in_tokens = inn.split()
                 out_tokens = out.split()
                 if len(in_tokens) != len(out_tokens):
@@ -93,34 +97,57 @@ class Model:
         return text_in[:n1], wrong_words[:n1], correct_words[:n1],\
                 text_in[n1:], wrong_words[n1:], correct_words[n1:]
 
-    # def construct_window_chars(self, sample, j):
-        
+    def construct_window_chars(self, sample, index):
+        win_chars = []
+        strr = " ".join(sample)
+        side = Model.WIN_CHARS // 2
 
+        for i in range(index - side, index + side + 1):
+            if i < 0 or i >= len(strr):
+                v1 = 0
+            elif ord(strr[i]) > Model.MAX_ALLOWED_CHAR:
+                v1 = 0
+            else:
+                v1 = ord(strr[i])
+            win_chars.append(v1)
+        return win_chars        
+    
     def construct_input(self, in_tokens_sent, in_ww):
-        self.fasttext = FastTextWrapper.load_fasttext_format(Model.FAST_TEXT)
+        if args.small_run == False:
+            self.fasttext = FastTextWrapper.load_fasttext_format(Model.FAST_TEXT)
         
         inputs_sent, in_emb_ww, chars_wind = [], [], []
         chars = list(in_tokens_sent)
 
         for i, sample in enumerate(in_tokens_sent):
             inn = np.zeros((Model.MAX_SENT_TOKENS, 300))
+            pos_token = 0
             for j, token in enumerate(sample):
-                try:
-                    inn[Model.MAX_SENT_TOKENS - i - 1][:] = np.float32(self.fasttext.wv[token])
-                except:
+                if args.small_run == True:
                     inn[Model.MAX_SENT_TOKENS - j - 1][:] = np.float32([0] * 300)
-                # if token == in_ww[i]:
-                #     char_w = self.construct_window_chars(sample, j)
+                else:
+                    try:
+                        inn[Model.MAX_SENT_TOKENS - i - 1][:] = np.float32(self.fasttext.wv[token])
+                    except:
+                        inn[Model.MAX_SENT_TOKENS - j - 1][:] = np.float32([0] * 300)
 
-            try:
-                w_emb = np.float32(self.fasttext.wv[token])
-            except:
+                if token == in_ww[i]:
+                    char_w = self.construct_window_chars(sample, pos_token)
+                pos_token += len(token) + 1
+
+            if args.small_run == True:
+                try:
+                    w_emb = np.float32(self.fasttext.wv[token])
+                except:
+                    w_emb = np.float32([0] * 300)
+            else:
                 w_emb = np.float32([0] * 300)
 
             in_emb_ww.append(w_emb)
             inputs_sent.append(inn)
+            chars_wind.append(char_w)
 
-        return [inputs_sent, in_emb_ww]
+        return [inputs_sent, in_emb_ww, chars_wind]
     
     def construct_output(self, train_cw, word2id):
         out = []
@@ -135,26 +162,44 @@ class Model:
 
         train_in, train_ww, train_cw, test_in, test_ww, test_cw = \
             self.split_dataset(text_in, wrong_words, correct_words)
-        print(len(train_in), len(train_ww))
+
         sentence_embeddings_layer = Input(shape=((Model.MAX_SENT_TOKENS, 300,)))
         sentence_lstm_layer = GRU(units=Model.GRU_CELL_SIZE, input_shape=(Model.MAX_SENT_TOKENS, 300,))
-        bi_lstm_layer = keras.layers.Bidirectional(layer=sentence_lstm_layer,\
+        bi_lstm_layer_sent = keras.layers.Bidirectional(layer=sentence_lstm_layer,\
 									merge_mode="concat")(sentence_embeddings_layer)
         word_emb = Input(shape=(300,))
-        conc = keras.layers.concatenate([bi_lstm_layer, word_emb], axis=-1)
+        if args.no_chars == False:
+            input_character_window = keras.layers.Input(shape=(Model.WIN_CHARS,))
+            character_embeddings_layer = keras.layers.Embedding(
+                                            input_dim=Model.MAX_ALLOWED_CHAR + 1,\
+                                            output_dim=Model.EMB_CHARS_SIZE)(input_character_window)
+            chars_lstm_layer = GRU(units=Model.GRU_CELL_SIZE, input_shape=(Model.MAX_CHARS_TOKENS, 300,))
+            bi_lstm_layer_chars = keras.layers.Bidirectional(layer=chars_lstm_layer,\
+                                        merge_mode="concat")(character_embeddings_layer)
+
+            conc = keras.layers.concatenate([bi_lstm_layer_sent, word_emb, bi_lstm_layer_chars], axis=-1)
+        else:
+            conc = keras.layers.concatenate([bi_lstm_layer_sent, word_emb], axis=-1)
+
         d1 = keras.layers.Dense(Model.DENSES[0], activation='tanh')(conc)                                                 
         output = keras.layers.Dense(voc_size, activation='softmax')(d1)
 
-        
         train_inn = self.construct_input(train_in, train_ww)
         train_out = self.construct_output(train_cw, word2id)
         callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=Model.PATIENCE)]
-        model = keras.models.Model(inputs=[sentence_embeddings_layer, word_emb],
+        if args.no_chars == True:
+            inn = [sentence_embeddings_layer, word_emb]
+        else:
+            inn = [sentence_embeddings_layer, word_emb, input_character_window]
+
+        model = keras.models.Model(inputs=inn,
 								   outputs=output)
         model.compile(optimizer='adam',\
                     loss='categorical_crossentropy',\
                     metrics=['accuracy'])
         print(model.summary())
+        if args.no_chars == True:
+            train_inn = [train_inn[0], train_inn[1]]
         model.fit(train_inn,
 				  [train_out],
 				  batch_size=Model.BATCH_SIZE, 
@@ -168,6 +213,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--small_run', dest='small_run', action='store_true', default=False)
     parser.add_argument('--name', dest="name", action="store", default="default")
+    parser.add_argument('--no_chars', dest="no_chars", action="store_true")
     args = parser.parse_args()
 
     for k in args.__dict__:
