c3c5e0547f4ca1d6f69a254f1b09840e2111da25
teo <teodor.cotet@gmail.com>
2019-08-31 15:47:44 +0300

    beam search noise

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -40,128 +40,15 @@ args = None
 log = open("log.log", "w", encoding='utf-8')
 
 
-
-# RNN "Cell" classes in Keras perform the actual data transformations at each timestep. Therefore, in order
-# to add attention to LSTM, we need to make a custom subclass of LSTMCell.
-class AttentionLSTMCell(LSTMCell):
-    def __init__(self, **kwargs):
-        self.attentionMode = False
-        super(AttentionLSTMCell, self).__init__(**kwargs)
-
-    # Build is called to initialize the variables that our cell will use. We will let other Keras
-    # classes (e.g. "Dense") actually initialize these variables.
-    @tf_utils.shape_type_conversion
-    def build(self, input_shape):
-        # Converts the input sequence into a sequence which can be matched up to the internal
-        # hidden state.
-        self.dense_constant = TimeDistributed(Dense(self.units, name="AttLstmInternal_DenseConstant"))
-
-        # Transforms the internal hidden state into something that can be used by the attention
-        # mechanism.
-        self.dense_state = Dense(self.units, name="AttLstmInternal_DenseState")
-
-        # Transforms the combined hidden state and converted input sequence into a vector of
-        # probabilities for attention.
-        self.dense_transform = Dense(1, name="AttLstmInternal_DenseTransform")
-
-        # We will augment the input into LSTMCell by concatenating the context vector. Modify
-        # input_shape to reflect this.
-        batch, input_dim = input_shape[0]
-        batch, timesteps, context_size = input_shape[-1]
-        lstm_input = (batch, input_dim + context_size)
-
-        # The LSTMCell superclass expects no constant input, so strip that out.
-        return super(AttentionLSTMCell, self).build(lstm_input)
-
-    # This must be called before call(). The "input sequence" is the output from the
-    # encoder. This function will do some pre-processing on that sequence which will
-    # then be used in subsequent calls.
-    def setInputSequence(self, input_seq):
-        self.input_seq = input_seq
-        self.input_seq_shaped = self.dense_constant(input_seq)
-        self.timesteps = tf.shape(self.input_seq)[-2]
-
-    # This is a utility method to adjust the output of this cell. When attention mode is
-    # turned on, the cell outputs attention probability vectors across the input sequence.
-    def setAttentionMode(self, mode_on=False):
-        self.attentionMode = mode_on
-
-    # This method sets up the computational graph for the cell. It implements the actual logic
-    # that the model follows.
-    def call(self, inputs, states, constants):
-        # Separate the state list into the two discrete state vectors.
-        # ytm is the "memory state", stm is the "carry state".
-        ytm, stm = states
-        # We will use the "carry state" to guide the attention mechanism. Repeat it across all
-        # input timesteps to perform some calculations on it.
-        stm_repeated = K.repeat(self.dense_state(stm), self.timesteps)
-        # Now apply our "dense_transform" operation on the sum of our transformed "carry state"
-        # and all encoder states. This will squash the resultant sum down to a vector of size
-        # [batch,timesteps,1]
-        combined_stm_input = self.dense_transform(
-            keras.activations.tanh(stm_repeated + self.input_seq_shaped))
-        # Performing a softmax generates a log probability for each encoder output to receive attention.
-        score_vector = keras.activations.softmax(combined_stm_input, 1)
-        # In this implementation, we grant "partial attention" to each encoder output based on
-        # it's log probability accumulated above. Other options would be to only give attention
-        # to the highest probability encoder output or some similar set.
-        context_vector = K.sum(score_vector * self.input_seq, 1)
-
-        # Finally, mutate the input vector. It will now contain the traditional inputs (like the seq2seq
-        # we trained above) in addition to the attention context vector we calculated earlier in this method.
-        inputs = K.concatenate([inputs, context_vector])
-
-        # Call into the super-class to invoke the LSTM math.
-        res = super(AttentionLSTMCell, self).call(inputs=inputs, states=states)
-
-        # This if statement switches the return value of this method if "attentionMode" is turned on.
-        if(self.attentionMode):
-            return (K.reshape(score_vector, (-1, self.timesteps)), res[1])
-        else:
-            return res
-
-# Custom implementation of the Keras LSTM that adds an attention mechanism.
-# This is implemented by taking an additional input (using the "constants" of the
-# RNN class) into the LSTM: The encoder output vectors across the entire input sequence.
-class LSTMWithAttention(RNN):
-    def __init__(self, units, **kwargs):
-        cell = AttentionLSTMCell(units=units)
-        self.units = units
-        super(LSTMWithAttention, self).__init__(cell, **kwargs)
-
-    @tf_utils.shape_type_conversion
-    def build(self, input_shape):
-        self.input_dim = input_shape[0][-1]
-        self.timesteps = input_shape[0][-2]
-        return super(LSTMWithAttention, self).build(input_shape)
-
-    # This call is invoked with the entire time sequence. The RNN sub-class is responsible
-    # for breaking this up into calls into the cell for each step.
-    # The "constants" variable is the key to our implementation. It was specifically added
-    # to Keras to accomodate the "attention" mechanism we are implementing.
-    def call(self, x, constants, **kwargs):
-        if isinstance(x, list):
-            self.x_initial = x[0]
-        else:
-            self.x_initial = x
-
-        # The only difference in the LSTM computational graph really comes from the custom
-        # LSTM Cell that we utilize.
-        self.cell._dropout_mask = None
-        self.cell._recurrent_dropout_mask = None
-        self.cell.setInputSequence(constants[0])
-        return super(LSTMWithAttention, self).call(inputs=x, constants=constants, **kwargs)
-
-        
 class Model:
     FAST_TEXT = "fasttext_fb/wiki.ro"
 
     """ params of the model """
     PATIENCE = 1000
-    EPOCHS = 10
-    BATCH_SIZE = 10
-    LATENT_DIM_RNN = 32
-    LATENT_DIM_CHARS = 16
+    EPOCHS = 5
+    BATCH_SIZE = 32
+    LATENT_DIM_RNN = 128
+    LATENT_DIM_CHARS = 25
     MAX_LENGTH_DECODER = 110
 
     """ max char to keep """
@@ -173,6 +60,7 @@ class Model:
     SRC_TEXT_CHAR_LENGTH = 200
     SMALL_RUN_SAMPLES = 100
     TRAIN_DEV_DATASET_PERCENTAGE = 0.97
+    BETA_RANDOM_NOISING = 0.1
 
     CORRECT_DIACS = {
         "ş": "ș",
@@ -315,16 +203,16 @@ class Model:
             target_seq = np.zeros((1, 1))
             target_seq[0, 0] = index
             saved_target_and_state = [target_seq] + state
-
+            cand_prob += Model.BETA_RANDOM_NOISING * random.randint(0, 100000) / 100000
             if len(new_beam_cands) < args.beam_width:
-                new_beam_cands[new_str] = (cand_prob * base_prob, saved_target_and_state) 
+                new_beam_cands[new_str] = (cand_prob + base_prob, saved_target_and_state) 
             else:
-                if cand_prob * base_prob > new_beam_cands[lowest_prob_key][0]:
+                if cand_prob + base_prob > new_beam_cands[lowest_prob_key][0]:
                     del new_beam_cands[lowest_prob_key]
                     
                     # Populate the first character of target sequence with the start character.
                     
-                    new_beam_cands[new_str] = (cand_prob * base_prob, saved_target_and_state) 
+                    new_beam_cands[new_str] = (cand_prob + base_prob, saved_target_and_state) 
 
     def compute_predictions(self, input_seq):
         """decoding works as follows:
@@ -501,7 +389,7 @@ class Model:
             states used later, encoder_h == encoder_lstm  (last hidden states == output)
             for full seq, call return_sequences==True
         """
-        encoder_lstm, encoder_h, encoder_c = LSTMWithAttention(units=Model.LATENT_DIM_RNN, return_state=True)(embedded_encoder)
+        encoder_lstm, encoder_h, encoder_c = LSTM(units=Model.LATENT_DIM_RNN, return_state=True)(embedded_encoder)
 
         encoder_states = [encoder_h, encoder_c]
         # decoder output model, we define a new Input because is recursive
@@ -509,7 +397,7 @@ class Model:
         char_emb_decoder = Embedding(input_dim=Model.NR_CHARS, output_dim=Model.LATENT_DIM_CHARS, mask_zero=True, trainable=True)
         embedded_decoder = char_emb_decoder(decoder_inputs)
         """ ret seququences return output (hidden) for each timestep, retur_state returns cell and hidden """
-        decoder_lstm = LSTMWithAttention(units=Model.LATENT_DIM_RNN, return_sequences=True, return_state=True)
+        decoder_lstm = LSTM(units=Model.LATENT_DIM_RNN, return_sequences=True, return_state=True)
         """ initial_state is the initial cell and initial hidden, which are initialized with the encoder output """
         decoder_outputs, _, _ = decoder_lstm(embedded_decoder, initial_state=encoder_states)
         decoder_dense = Dense(Model.NR_CHARS, activation='softmax')
@@ -539,7 +427,9 @@ class Model:
                 print(outc.shape, file=log)
                 break
 
-        callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=Model.PATIENCE)]
+        filepath = "models/saved-model-{epoch:02d}-{val_loss:.2f}.hdf5"
+        checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+        callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=Model.PATIENCE), checkpoint]
         print(model.summary())
         
         encoder_input_data = np.asarray(encoder_input_data) 
@@ -576,7 +466,16 @@ class Model:
             input_str = "".join([chr(code_point) for code_point in input_seq])
             print(input_str, file=log)
             self.compute_predictions(input_seq)
-
+        
+        print('Test ----------------', file=log)
+        encoder_input_data, decoder_input_data, decoder_output_data = \
+            self.construct_input_output_chars(raw_in=test_raw_out, raw_out=test_raw_out, 
+                                                max_size=Model.SRC_TEXT_CHAR_LENGTH)
+        """ run on 20 examples from train to see if its okay"""
+        for input_seq in encoder_input_data[:20]:
+            input_str = "".join([chr(code_point) for code_point in input_seq])
+            print(input_str, file=log)
+            self.compute_predictions(input_seq)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--small_run', dest='small_run', action='store_true', default=False)
