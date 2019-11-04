aea5451c5aded6b1c521fdfffddc92b0f9fa301c
teo <teodor.cotet@gmail.com>
2019-11-04 12:06:15 +0200

    decode script

--- t2t/decode.sh
+++ t2t/decode.sh
@@ -7,18 +7,14 @@ MODEL=transformer
 HPARAMS=transformer_tiny
 TRAIN_DIR=$PWD/gec_ro/train/$MODEL-$HPARAMS
 mkdir -p $TRAIN_DIR $TMP_DIR $DATA_DIR
-DECODE_FILE=$DATA_DIR/decode_this.txt
+DECODE_FILE=$DATA_DIR/decode_this_sample.txt
 RESULTS_DECODE_FILE=test_corrected_wiki.txt
+touch $RESULTS_DECODE_FILE
 
-
-echo "Muzica nu are o definitie propriuzisa ." >> $DECODE_FILE
-echo "Sigur însă că în RDG a existat pînă în 1990 alt imn." >> $DECODE_FILE
-echo "Aceasta este situaţia şi până în ziua de azi, în Germania reunită." >> $DECODE_FILE
-echo "Lidul devine imn național oficial german abia în 1922." >> $DECODE_FILE
-echo "Cântecul germanilor este imnul național al Germaniei" >> $DECODE_FILE
 
 BEAM_SIZE=4
 ALPHA=0.6
+
 
 t2t-decoder \
   --t2t_usr_dir=$USR_DIR \
