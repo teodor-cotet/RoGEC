8d8f4509dc8ca62edbdc15eafc51e7379aba26c6
teo <teodor.cotet@gmail.com>
2020-04-06 17:02:39 +0300

    tpu custom training loop 9

--- transformer_bert.py
+++ transformer_bert.py
@@ -451,6 +451,7 @@ def main(argv):
         tf.config.experimental_connect_to_cluster(tpu_cluster_resolver)
         tf.tpu.experimental.initialize_tpu_system(tpu_cluster_resolver)
         strategy = tf.distribute.experimental.TPUStrategy(tpu_cluster_resolver)
+        strategy.experimental_enable_dynamic_batch_size = False
         print('Running on TPU ', tpu_cluster_resolver.cluster_spec().as_dict()['worker'])
         with strategy.scope():
             if args.test:
