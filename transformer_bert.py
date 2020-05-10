de45711751da539173deaea6dc41f1097fc70607
teo <teodor.cotet@gmail.com>
2020-05-10 18:51:38 +0300

    loss/acc fixed for tpu

--- transformer_bert.py
+++ transformer_bert.py
@@ -311,7 +311,8 @@ def loss_function(real, pred):
 
     mask = tf.cast(mask, dtype=tf.float32)
     loss_ *= mask
-    loss_reduced = tf.math.divide(tf.reduce_sum(loss_), tf.reduce_sum(mask))
+    # loss_reduced = tf.math.divide(tf.reduce_sum(loss_), tf.reduce_sum(mask))
+    loss_reduced = tf.reduce_sum(loss_)
     return loss_reduced
 
 def acc_function(real, pred):
@@ -323,7 +324,8 @@ def acc_function(real, pred):
 
     mask = tf.math.logical_not(tf.math.equal(real, 0))
     mask = tf.cast(mask, tf.int64)
-    accuracy = tf.math.divide(tf.reduce_sum(eq * mask), tf.reduce_sum(mask))
+    # accuracy = tf.math.divide(tf.reduce_sum(eq * mask), tf.reduce_sum(mask))
+    accuracy = tf.reduce_sum(eq * mask)
     return accuracy
 
 @tf.function(input_signature=train_step_signature)
