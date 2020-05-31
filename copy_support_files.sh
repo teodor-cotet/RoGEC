f584b83d6cb3fb02e44eb6e6f0192be9aa70582b
teo <teodor.cotet@gmail.com>
2020-05-31 18:44:32 +0300

    refactored

--- copy_support_files.sh
+++
@@ -1,6 +0,0 @@
-echo "copying files to $1"
-scp checkpoints/10m_transformer_768/tokenizer_ro.subwords teodor.cotet@$1:~/RoGEC/checkpoints/10m_transformer_768/
-scp checkpoints/10m_transformer_768/tokenizer_ro.subwords teodor.cotet@$1:~/RoGEC/checkpoints/10m_transformer_768_finetune/
-scp checkpoints/10m_transformer_768/tokenizer_ro.subwords teodor.cotet@$1:~/RoGEC/checkpoints/10m_transformer_768_retrain/
-scp  -r corpora/cna/ teodor.cotet@$1:~/RoGEC/corpora/
-scp "/media/teo/drive hdd/gec/corpora/wiki_synthetic/arpa/30m_wiki_clean.zip" teodor.cotet@$1:~/RoGEC/corpora/
